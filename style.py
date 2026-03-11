b8c0c7aaa7455b87d4a832a47612d9e757d5b671
AIM-Core <aim-core@mac.lan>
2026-03-11 19:13:39 +0100

    110326 XIII - neues layout typo 4

--- style.py
+++ style.py
@@ -35,7 +35,7 @@ def apply_custom_style():
         /* 2. INPUT FELDER (Weißer Hintergrund, schwarze Schrift) */
         .stTextInput input, .stTextArea textarea, .stSelectbox div[data-baseweb="select"] {
             background-color: #FFFFFF !important;
-            color: #DDDDDD; !important;
+            color: #7F7F7F; !important;
             border: 1px solid var(--aim-gray-medium) !important;
         }
         
